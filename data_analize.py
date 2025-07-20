479f10fe4d2780474547324fa3b5834b5b22c60d
Khadijeh Shirvani <khadijehshirvani7@gmail.com>
2025-07-21 02:15:50 +0330

    Update data_analize.py

--- data_analize.py
+++ data_analize.py
@@ -1,12 +1,1 @@
 
-
-import pandas as pd 
-import csv
-import sys
-
-
-Away_team=pd.read_csv("merged_away_team_data.csv")
-home_team=pd.read_csv("merged_home_team_data.csv")
-all_players=pd.concat([Away_team["player_id"],home_team["player_id"]],axis=0)
-print("The number of tennis players in the dataset is: ", len(all_players.unique()))
-print(all_players)
